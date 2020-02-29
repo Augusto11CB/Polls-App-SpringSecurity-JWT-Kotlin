bc5d9a4b3c00bb8a9366c5d2596fcbd78c6a068c
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-02-29 15:47:44 -0300

    First Commit

---
+++ src/main/kotlin/spring/studies/polls/PollsApplication.kt
@@ -0,0 +1,11 @@
+package spring.studies.polls
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PollsApplication
+
+fun main(args: Array<String>) {
+	runApplication<PollsApplication>(*args)
+}
---
+++ src/test/kotlin/spring/studies/polls/PollsApplicationTests.kt
@@ -0,0 +1,13 @@
+package spring.studies.polls
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PollsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
