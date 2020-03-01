69c4050eaf19dc39eb06a2f2f18c3a0fb72f4984
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-03-01 18:27:06 -0300

    Config Spring security

--- PollsApp/src/main/kotlin/spring/studies/polls/config/SecurityConfig.kt
+++ PollsApp/src/main/kotlin/spring/studies/polls/config/SecurityConfig.kt
@@ -1,7 +1,5 @@
 package spring.studies.polls.config
 
-import com.example.polls.security.JwtAuthenticationEntryPoint
-import com.example.polls.security.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -13,8 +11,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import spring.studies.polls.security.JWTAuthenticationEntryPoint
+import spring.studies.polls.security.JWTAuthenticationFilter
 import spring.studies.polls.service.CustomUserDetailsService
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+
+
 
 
 @Configuration
@@ -25,33 +30,42 @@ import spring.studies.polls.service.CustomUserDetailsService
         securedEnabled = true
 )//This is used to enable method level security based on annotations.
 data class SecurityConfig(
-        val customUserDetailsService : CustomUserDetailsService, //To authenticate a User or perform various role-based checks, Spring security needs to load users details
-        val unauthoziredHandler : JwtAuthenticationEntryPoint // This class is used to return a 401 unauthorized error to clients that try to access a protected resource without proper authentication
-): WebSecurityConfigurerAdapter() {
-                // Enable the Spring Security Filter Chain
-                // This chain allow us filter certain requests so that user doesn't have to be authenticated (Ex: To access CSS)
+        val customUserDetailsService: CustomUserDetailsService, //To authenticate a User or perform various role-based checks, Spring security needs to load users details
+        val unauthoziredHandler: JWTAuthenticationEntryPoint // This class is used to return a 401 unauthorized error to clients that try to access a protected resource without proper authentication
+) : WebSecurityConfigurerAdapter() {
+    // Enable the Spring security Filter Chain
+    // This chain allow us filter certain requests so that user doesn't have to be authenticated (Ex: To access CSS)
 
 
     @Bean
-    fun jwtAuthenticationFilter(): JwtAuthenticationFilter {
-        return JwtAuthenticationFilter()
+    fun jwtAuthenticationFilter(): JWTAuthenticationFilter {
+        return JWTAuthenticationFilter()
 
         /*
         * reads JWT authentication token from the Authorization header of all the requests
         * validates the token
         * loads the user details associated with that token.
-        * Sets the user details in Spring Security’s SecurityContext. Spring Security uses the user details to perform authorization checks.
+        * Sets the user details in Spring security’s SecurityContext. Spring security uses the user details to perform authorization checks.
         * */
     }
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
-        auth.userDetailsService(customUserDetailsService)
-                .pass
+        auth!!
+            .userDetailsService(customUserDetailsService) //TODO Maybe it doesn't work because I have created a interface that extands UserDetailService
+            .passwordEncoder(passwordEncoder())
 
-    /*
-    *
-    * AuthenticationManagerBuilder is used to create an AuthenticationManager
-    * */
+        /*
+        *
+        * AuthenticationManagerBuilder is used to create an AuthenticationManager.
+        *
+        * It can be used to:
+        * build in-memory authentication, LDAP authentication, JDBC authentication, or add your custom authentication provider.
+        * */
+    }
+
+    @Bean
+    public fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
     }
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
@@ -61,7 +75,7 @@ data class SecurityConfig(
 
         /*
         *
-        * AuthenticationManager is the main Spring Security interface for authenticating a user.
+        * AuthenticationManager is the main Spring security interface for authenticating a user.
         *
         * */
     }
@@ -75,7 +89,7 @@ data class SecurityConfig(
                 .csrf()
                     .disable()
                 .exceptionHandling()
-                    .authenticationEntryPoint(unauthorizedHandler)
+                    .authenticationEntryPoint(unauthoziredHandler)
                     .and()
                 .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -97,11 +111,19 @@ data class SecurityConfig(
                         .permitAll()
                     .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
                         .permitAll()
+                    .antMatchers("/h2/**")
+                        .permitAll()
                     .anyRequest()
                         .authenticated()
-
         // Add our custom JWT security filter
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
 
+    }
+
+    @Throws(Exception::class)
+    override fun configure(web: WebSecurity?) {
+        web!!
+                .ignoring()
+                .antMatchers("/h2/**")
     }
 }
